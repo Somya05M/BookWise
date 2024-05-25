880489d5f882467ef262523f35c06a11f816ae5c
Somya Malgudi <Somya.Malgudi@iiitb.ac.in>
2024-05-25 16:42:02 +0530

    Testing Pipeline throught Git SCM polling 1

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BookWis</span>
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BookWise</span>
         </a>
         <button 
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
